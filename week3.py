108d5e42b68634f4ff97f59e0ca0aa522f1d3a76
ydakkak <43996523+ydakkak@users.noreply.github.com>
2018-10-09 15:43:57 +0100

    Add files via upload

---
+++ week3.py
@@ -0,0 +1,21 @@
+'''x=int(input("please enter  first number "))
+y=int(input("please enter scound number "))
+print (x+y)
+--------
+num=4
+print (num)
+num*=2
+print (num) 
+num1=num+2
+print (num1) 
+num1+=3
+print (num1)
+---------
+x=2
+y=3
+print (x/3)
+print (x//y)'''
+
+
+
+
